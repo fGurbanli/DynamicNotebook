452e7e34a5a3875c0660571493e0f3a908ebff05
Furgan Gurbanli <f.qurbanli.2006@gmail.com>
2026-07-14 10:54:46 +0400

    PrintDatas()

--- main.c
+++ main.c
@@ -1,39 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int GetUserInput(int* ptr, int cnt);
-void PrintingDatas(int* ptr, int cnt);
+int GetUserInput(int cnt);
+void PrintDatas(int* ptr, int cnt);
 
 int main(void) {
     int capacity = 2;
     int count = 0;
-    int* input;
+    int input;
     int* datas = calloc(capacity, sizeof(int));
     if (datas == NULL) {
         printf("\nMemory allocation failed!");
         return 1;
     }
-    while (GetUserInput(input, count) != -1)
+    while (1)
     {
+        input = GetUserInput(count);
+        if (input == -1)
+        {
+            break;
+        }
         if (count >= capacity)
         {
             capacity *= 2;
             int* temp = realloc(datas, capacity * sizeof(int));
             if (temp == NULL) {
                 printf("\nMemory allocation failed!");
-                free(temp);
+                free(datas);
                 return 1;
             }
             datas = temp;
         }
-        datas[count] = *input;
+        datas[count] = input;
         count++;
     }
+    realloc(datas, count*sizeof(int));
+    PrintDatas(datas, count);
     free(datas);
     return 0;
 }
 
-int GetUserInput(int* ptr, int cnt)
+int GetUserInput(int cnt)
 {
     printf("Please enter a value for Data[%d]\n", cnt);
     int input;
@@ -46,8 +53,15 @@ int GetUserInput(int* ptr, int cnt)
         while (getchar() != '\n');
         printf("Please enter valid value!\n");
     }
-    *ptr = input;
     return input;
 }
 
-
+void PrintDatas(int* ptr, int cnt)
+{
+    printf("Here is your datas:\n");
+    for (int i = 0; i < cnt; i++)
+    {
+        printf(" %d",*ptr);
+        ptr++;
+    }
+}
